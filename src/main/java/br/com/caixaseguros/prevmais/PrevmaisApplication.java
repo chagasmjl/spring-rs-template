91739129907fee4d712b253f61580598a0c21ba8
mario <chagasmjl@gmail.com>
2018-07-15 22:05:11 -0300

    initial commit

---
+++ src/main/java/br/com/caixaseguros/prevmais/PrevmaisApplication.java
@@ -0,0 +1,12 @@
+package br.com.caixaseguros.prevmais;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrevmaisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrevmaisApplication.class, args);
+	}
+}

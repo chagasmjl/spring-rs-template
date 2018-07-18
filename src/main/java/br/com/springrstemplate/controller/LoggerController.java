da8bfb4f71c0f0f82b0bad965d9fb81e18cb370c
mario <chagasmjl@gmail.com>
2018-07-17 23:31:09 -0300

    adding tests

--- src/main/java/br/com/springrstemplate/controller/LoggerController.java
+++ src/main/java/br/com/springrstemplate/controller/LoggerController.java
@@ -3,23 +3,23 @@ package br.com.springrstemplate.controller;
 import java.util.Collection;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.springrstemplate.LogRepository;
 import br.com.springrstemplate.entity.Log;
 
 @RestController
-@RequestMapping("/log/")
 public class LoggerController {
 	
 	@Autowired
 	private LogRepository rep;
 	
-	@GetMapping
+
+	@RequestMapping(name="/logs",method=RequestMethod.GET)
 	public Collection<Log> getLog() {
 		return rep.findAll();
 		
--- src/test/java/br/com/springrstemplate/PrevmaisApplicationTests.java
+++ src/test/java/br/com/springrstemplate/PrevmaisApplicationTests.java
@@ -1,16 +1,26 @@
-package br.com.caixaseguros.prevmais;
+package br.com.springrstemplate;
+
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import br.com.springrstemplate.controller.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class PrevmaisApplicationTests {
 
+	@Autowired
+	private LoggerController loggerController;
+	
 	@Test
 	public void contextLoads() {
+		assertThat(loggerController).isNotNull() ;
 	}
 
 }

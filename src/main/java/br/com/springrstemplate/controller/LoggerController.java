53f54e2756f94c58d9a844159a4cc105804cf2b5
Mario Jorge Lopes Chagas de Almeida <chagasmjl@gmail.com>
2018-08-03 10:06:48 -0300

    local changes

--- src/main/java/br/com/springrstemplate/controller/LoggerController.java
+++ src/main/java/br/com/springrstemplate/controller/LoggerController.java
@@ -3,6 +3,7 @@ package br.com.springrstemplate.controller;
 import java.util.Collection;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -32,6 +33,7 @@ public class LoggerController {
 	
 	/** POST	 */
 	@PostMapping
+	@Async
 	public void saveLog(@RequestBody Log log) {
 		rep.save(log);
 	}
